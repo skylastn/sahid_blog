7fd950fb90d931bf27756049fbc1556a3089c093
Sahid Rahutomo <sahidrahutomo@gmail.com>
2021-01-13 16:23:23 +0700

    Initial commit

---
+++ lib/config/constants.dart
@@ -0,0 +1,20 @@
+mixin AppConstants {
+  static final String mail = 'sahidrahutomo@gmail.com';
+
+  static final String phone = '(+62)8815123766';
+
+  static final String location = 'Klaten, Yogyakarta, Bandung, Solo, Semarang';
+
+  static final String github = 'https://github.com/skylastn';
+
+  static final String linkedin =
+      'https://www.linkedin.com/in/sahid-rahutomo-0145391a5/';
+
+  static final String twitter = 'https://twitter.com/MedAbdelouahed';
+
+  static final String facebook =
+      'https://www.facebook.com/rizkiwahyu.hutomo/';
+
+  static final String cv =
+      'https://drive.google.com/file/d/1VEOJD-53fSksYDbYfC8gKmsn1-yHWbva/view?usp=sharing';
+}
---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'ui/home.dart';
+import 'config/colors.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Sahid Blog',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        accentColor: AppColors.yellow,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
